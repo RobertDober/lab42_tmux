592bea80b3dfd607a7cd58cd74129745b774b26f
Robert Dober <robert.dober@gmail.com>
2013-10-01 19:41:49 +0200

    routine bup

--- spec/extensions/file_spec.rb
+++ spec/extensions/file_spec.rb
@@ -33,11 +33,29 @@ describe File::YAML, :wip do
       before do
         stub_files "xxx.yml"
       end
+      it "returns it for the exact name" do
+        expect( subject.exists? "xxx.yml" ).to eq( "xxx.yml" )
+      end
+      it "returns it for the name without extension" do
+        expect( subject.exists? "xxx" ).to eq( "xxx.yml" )
+      end
+      it "returns nil for .yaml name" do
+        expect( subject.exists? "xxx.yaml" ).to be_false
+      end
     end # context "the .yml file exists"
     
     context "the .yaml and .yml file exists" do
       before do
         stub_files "xxx.yaml", "xxx.yml"
+      end
+      it "returns it for the exact name" do
+        expect( subject.exists? "xxx.yaml" ).to eq( "xxx.yaml" )
+      end
+      it "returns it for the name without extension" do
+        expect( subject.exists? "xxx" ).to eq( "xxx.yaml" )
+      end
+      it "returns .yml for the yml name" do
+        expect( subject.exists? "xxx.yml" ).to eq( "xxx.yml" )
       end
     end # context "the .yaml and .yml file exists"
     
